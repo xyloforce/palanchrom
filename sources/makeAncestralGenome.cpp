0c6b6787e06be7023d5110cf1f4ef7af0a00eed7
xyloforce <fabsassolas@outlook.fr>
2021-11-08 16:39:56 +0100

    fix

--- sources/makeAncestralGenome.cpp
+++ sources/makeAncestralGenome.cpp
@@ -31,33 +31,31 @@ int main(int argc, char* argv[]) {
         // get seq
         // fill seq
         
-        fasta_entry entry2 = entry;
-        std::string N = "";
-        for(int i(0); i< entry2.getSize(); i++) {
-            N += 'N';
+        std::string sequence;
+        //fasta_entry entry2 = entry;
+        // std::string N = "";
+        for(int i(0); i< entry.getSize(); i++) {
+            sequence += 'N';
         }
         
-        entry2.editSeq(N, 0, entry2.getSize());
+        // entry2.editSeq(N, 0, entry2.getSize());
         
-        std::map <int, bed_entry> currentInt = intervals.getBedByID(entry2.getChrom());
+        std::map <int, bed_entry> currentInt = intervals.getBedByID(entry.getChrom());
         std::cout << "Checking int..." << currentInt.size() << " intervals left" << std::endl;
         int count = 0;
 
         for(const auto &pair : currentInt) {
             count ++;
-            entry2.editSeq(
-                entry.subsetEntry(pair.second.getStart(), pair.second.getStop()).getSequence(),
-                           pair.second.getStart(),
-                           pair.second.getStop());
-            
-            std::cout << count << std::endl;
-            if(count % 100 == 0) {
+            int size(pair.second.getStop()-pair.second.getStart());
+            sequence.replace(pair.second.getStart(),  size, entry.subsetEntry(pair.second.getStart(), pair.second.getStop()).getSequence());
+
+            if(count % 1000 == 0) {
                 std::cout << count << "         \r";
             }
         }
 
         count = 0;
-        std::vector <vcf_entry> currentVCF = mutations.getVCFByID(entry2.getChrom());
+        std::vector <vcf_entry> currentVCF = mutations.getVCFByID(entry.getChrom());
         std::cout << "Checking mutations..." << currentVCF.size() << " mutations left" << std::endl;
         for(const auto &VCFentry : currentVCF) {
             temp = "";
@@ -65,12 +63,13 @@ int main(int argc, char* argv[]) {
             // check old base
             int posMut = VCFentry.getPos();
             posMut --; // bc vcf are 1-based
-            std::cout<< entry2.getSequence() << std::endl;
-            if(entry2.subsetEntry(posMut, posMut+1).getSequence() == VCFentry.get_ref()) {
-                temp += VCFentry.get_alternate();
-                entry2.editSeq(temp, posMut, posMut +1);
+            std::string strBase = "";
+            strBase += sequence[posMut];
+
+            if(strBase == VCFentry.get_ref()) {
+                sequence[posMut] = VCFentry.get_alternate();
             } else {
-                std::cout << "Ref is : " << VCFentry.get_ref() << " and current is : " << entry2.subsetEntry(posMut, posMut+1).getSequence() << " at pos : " << posMut << std::endl;
+                std::cout << "Ref is : " << VCFentry.get_ref() << " and current is : " << sequence[posMut] << " at pos : " << posMut << std::endl;
                 throw std::logic_error("Probable index issue");
             }
             if(count % 100 == 0) {
@@ -78,7 +77,8 @@ int main(int argc, char* argv[]) {
             }
             
         }
-        outputFasta.write_fasta_entry(entry2);
+        entry.editSeq(sequence, 0, entry.getSize());
+        outputFasta.write_fasta_entry(entry);
     }
     
     return 0;
