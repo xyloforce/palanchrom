20514896e9f64eb25c0549858f5994b3b5bd5438
xyloforce <fabsassolas@outlook.fr>
2022-01-06 14:23:26 +0100

    corrected mutation detection

--- sources/getAncestralBase.cpp
+++ sources/getAncestralBase.cpp
@@ -8,8 +8,8 @@
 
 int main(int argc, char* argv[])
 {
-    if(argc < 4) {
-        throw std::domain_error("Need at last three args : fasta1, fasta2, vcf output");
+    if(argc < 5) {
+        throw std::domain_error("Need at last three args : ref fasta 1, ref fasta 2, outgroup fasta 3, vcf output");
     }
     // defines array of fasta objects
     fasta files[argc -2];
@@ -17,56 +17,73 @@ int main(int argc, char* argv[])
     fasta_entry entries[argc - 2];
     vcf outputFile(argv[argc - 1], false);
     int count = 0;
-    
+
     std::cout << "Opening files..." << std::endl;
     // open files
     for (int arg(1); arg < argc -1; arg++) {
         files[arg - 1] = fasta(argv[arg], "read_line", true);
     }
-    
+
     std::cout << "Starting analysis..." << std::endl;
-    
+
     while(!files[0].isEOF()) {
         // read one fasta entry for each file
         for(int i(0); i < argc -2; i ++) {
             entries[i] = files[i].readFastaLine();
         }
-        // now you need to compare each base of sequences
-        // for this you need to know if outgroups is defined
-        // then you need to check if first one is ok or not
-        std::string consensus = entries[1].getSequence();
+
+        std::string consensus = entries[2].getUppercaseSequence();
         std::string tstring;
-        for(unsigned int i(1); i < argc - 2; i++) {
-            tstring = entries[i].getSequence();
+        for(unsigned int i(2); i < argc - 2; i++) { // second file has "half-ref" status
+            tstring = entries[i].getUppercaseSequence();
             for (unsigned long j(0); j < tstring.size(); j++) {
-                if(toupper(tstring[j]) != toupper(consensus[j])) {
+                if(tstring[j] != consensus[j]) {
                     consensus[j] = 'N';
                 }
             }
         }
-        // now we have a string with "N" if not common
-        std::string ref = entries[0].getSequence();
+        std::string ref = entries[0].getUppercaseSequence();
+        std::string ref2 = entries[1].getUppercaseSequence();
         for(unsigned long i(0); i<ref.size(); i++) {
-            if(toupper(consensus[i]) != toupper(ref[i]) && toupper(ref[i]) != 'N') { // no need to consider N on the ref side bc there already muted
-                // write mutation in file
-                std::string refS = "";
-                char commonBase = consensus[i];
+            if(consensus[i] != ref[i] && ref[i] != 'N') {
+                std::string refS("");
+                std::string info(".");
+                char commonBase('\0');
+
+                if(consensus[i] == ref2[i]) {
+                    commonBase = consensus[i];
+                    if(entries[0].getStrand() == '-') {
+                        refS += reverseComp(ref[i]);
+                        commonBase = reverseComp(commonBase);
+                    } else {
+                        refS += ref[i];
+                    }
+                }
+                else if(consensus[i] != 'N') {
+                    commonBase = 'N';
+                    info = "source=non-match;ref=";
+                    info += consensus[i];
+                    info += ";alt=";
+                    info += ref2[i];
+                    info += ";";
+                    if(entries[0].getStrand() == '-') {
+                        refS += reverseComp(ref[i]);
+                    } else {
+                        refS += ref[i];
+                    }
+                }
+                else {
+                    commonBase = 'N';
+                    if(entries[0].getStrand() == '-') {
+                        refS += reverseComp(ref[i]);
+                    } else {
+                        refS += ref[i];
+                    }
+                }
                 std::vector <std::string> alternate(1);
                 alternate[0] += commonBase;
-                if(entries[0].getStrand() == '-') {
-                    refS += reverseComp(ref[i]);
-                    commonBase = reverseComp(commonBase);
-                } else {
-                    refS += ref[i];
-                }
-
-                outputFile.vcf_writeline(vcf_entry(entries[0].getChrom(), entries[0].getPos(i), ".", refS, alternate));
+                outputFile.vcf_writeline(vcf_entry(entries[0].getChrom(), entries[0].getPos(i), ".", refS, alternate, 0, ".", info));
             }
         }
-        count ++;
-        if(count % 1000 == 0) {
-            std::cout << count << "                    \r";
-        }
     }
-    return 0;
 }
