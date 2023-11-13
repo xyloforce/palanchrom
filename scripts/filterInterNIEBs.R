f2ee2e23e830644f47633aeef6d332001eda3b6e
xyloforce <fabsassolas@outlook.fr>
2023-11-13 13:52:53 +0100

    added filtering based on max size

--- scripts/filterInterNIEBs.R
+++ scripts/filterInterNIEBs.R
@@ -8,10 +8,17 @@ data = read.delim(args[1], header = FALSE)
 if(length(args) > 2) {
     min_intra = as.numeric(args[3])
     min_extra = as.numeric(args[4])
-    print(min_intra)
 } else {
     min_extra = 0
     min_intra = 0
+}
+
+if(length(args) > 4) {
+    max_intra = as.numeric(args[5])
+    max_extra = as.numeric(args[6])
+} else {
+    max_intra = Inf
+    max_extra = Inf
 }
 
 # data = read_tsv("../Data/20200228_interSmallNFR.dat", col_names = FALSE)
@@ -26,13 +33,15 @@ right = data.frame("chrom" = data$V1,
                    "end" = floor((data$V4 + data$V5) / 2),
                    "strand" = "R", "zero" = (data$V4))
 
-Left = left[(left$zero - left$start) > (min_intra * -1), ]
-Left = Left[(Left$end - Left$zero) > min_extra, ]
+left = left[(left$zero - left$start) > (min_intra * -1), ]
+left = left[(left$end - left$zero) > min_extra, ]
+left = left[(left$end - left$zero) < max_extra, ]
 
-Right = right[(right$zero - right$start) > min_extra, ]
-Right = Right[(Right$end - Right$zero) > (min_intra * -1), ]
+right = right[(right$zero - right$start) > min_extra, ]
+right = right[(right$end - right$zero) > (min_intra * -1), ]
+right = right[(right$end - right$zero) < (max_extra * -1), ]
 
-output = rbind(Left, Right)
+output = rbind(left, right)
 output = output[order(output$chrom, output$start), ]
 
 ## put it in bed form
